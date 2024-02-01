c8535664208635ff7e24fa120851e6882c7c350f
risv1 <rishi.aangarai@gmail.com>
2024-02-01 23:10:04 +0530

    refactor: go setup

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"file-stash/src/routes"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main(){
+	app := fiber.New();
+
+    routes.Setup(app)
+
+    app.Listen(":8000")
+}
---
+++ src/routes/routes.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"file-stash/src/handlers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func Setup(app *fiber.App){
+	app.Get("/", handlers.HelloWorld)
+}
