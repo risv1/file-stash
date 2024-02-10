b217eb1d1b3461a8a694b522d06e99ae90e77581
risv1 <rishi.aangarai@gmail.com>
2024-02-10 17:13:33 +0530

    feat: fetch files

--- client/src/app/components/pages/view/view.component.ts
+++ client/src/app/components/pages/view/view.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { Storage, getDownloadURL, listAll, ref } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-view',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './view.component.html',
   styleUrl: './view.component.scss'
 })
 export class ViewComponent {
+  files: { fileName: string, fileUrl: string }[] = [];
+  storage = inject(Storage);
+  
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    const listRef = ref(this.storage, '');
+    listAll(listRef)
+      .then((result: { items: any[]; }) => {
+        result.items.forEach(async itemRef => {
+          const url = await getDownloadURL(itemRef);
+          this.files.push({ fileName: itemRef.name, fileUrl: url });
+        });
+        console.log("Files:", this.files);
+      })
+      .catch((error: any) => {
+        console.log("Error:", error);
+      })
+  }
 
 }
